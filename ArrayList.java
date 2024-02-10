ef43c49c7570d00e692d9e9ffd4476d92514dc83
Nilesh Mittha <100112399+nileshmittha@users.noreply.github.com>
2024-02-10 20:25:56 +0530

    Add files via upload

---
+++ ArrayList.java
@@ -0,0 +1,15 @@
+//
+//import java.util.*;
+//import java.io.*;
+//import java.lang.reflect.Array;
+//
+//
+//
+//public class ArrayList {
+//
+//    public static void main(String[] args) {
+////        ArrayList<Integer> l1=new ArrayList<Integer>();
+//        ArrayList<Integer> arr1 = new ArrayList<Integer>(n);
+////        l1.add(54);
+//    }
+//}
