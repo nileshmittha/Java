b7163ad90bc4fde085342b1fe69252c9f7082aeb
Nilesh Mittha <100112399+nileshmittha@users.noreply.github.com>
2024-05-25 00:03:29 +0530

    Add files via upload

---
+++ collectionsFile/HashSet1.java
@@ -0,0 +1,25 @@
+package collectionFramework.HashSet;
+
+import java.util.Collections;
+import java.util.HashSet;
+
+public class HashSet1 {
+
+    public static void main(String[] args) {
+
+
+        HashSet s1=new HashSet();
+        s1.add(12);
+        s1.add(45);
+        s1.add(56);
+        s1.add(67);
+        s1.add(78);
+
+        System.out.println(s1);
+
+
+
+
+
+    }
+}
